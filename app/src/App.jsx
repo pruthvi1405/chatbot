cc2dcd8a79b776305243be1696df583fe2411b15
Pruthvi <niranjan.pr@northeastern.edu>
2024-07-01 17:11:44 -0400

    generating response from chatgpt

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,8 +3,13 @@ import './App.css';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
+
 function App() {
+
+  const API_KEY="sk-proj-vA5i98x2ighLXuHBS82sT3BlbkFJ3UkqXIhgnp8Tp7GrCtrD";
+
   const [typing, setTyping] = useState(false);
+
   const [messages, setMessages] = useState([
     {
       message: "Hello, I am ChatGPT",
@@ -19,27 +24,65 @@ function App() {
       sender: "user",
       direction: "outgoing"
     };
-
-    setMessages(prevMessages => [...prevMessages, newMessage]);
+    const newMessages=[...messages,newMessage]
+    console.log("newMessages :",newMessages );
+    setMessages(newMessages);
     setTyping(true);
-
-    // Simulating a response delay
-    setTimeout(() => {
-      const chatGPTMessage = {
-        message: "This is a response from ChatGPT.",
-        sender: "ChatGPT",
-        direction: "incoming"
-      };
-      setMessages(prevMessages => [...prevMessages, chatGPTMessage]);
-      setTyping(false); // Set typing indicator to false after receiving a response
-    }, 10000);
+    await processMessagetoChatGPT(newMessages)
   };
+
+  async function processMessagetoChatGPT(chatMessages){
+      let apiMessage=chatMessages.map((messageObject)=>{
+        let role="";
+        if (messageObject.sender==="ChatGPT"){
+          role="assistant"
+        }else{
+          role="user"
+        }
+        return {role:role,content:messageObject.message}
+      })
+
+      const systemMessage={
+        role:"system",
+        content:"Speak like a travel Agent"
+      }
+
+      const apiRequestBody={
+        "model":"gpt-3.5-turbo",
+        "messages":[
+          systemMessage,
+          ...apiMessage
+        ]
+      }
+
+      await fetch("https://api.openai.com/v1/chat/completions",{
+        method:"POST",
+        headers:{
+          "Authorization":"Bearer " + API_KEY,
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify(apiRequestBody)
+      }).then((data)=>{
+        return data.json();
+      }).then((data)=>{
+        console.log(data.choices[0].message.content);
+        setMessages(
+          [...chatMessages,{
+            message:data.choices[0].message.content,
+            sender:"ChatGPT",
+            direction: "incoming"
+          }]
+        )
+        setTyping(false)
+      })
+  }
+
 
   return (
     <div style={{ position: "relative", height: "800px", width: "700px" }}>
       <MainContainer>
         <ChatContainer>
-          <MessageList typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing.." /> : null }>
+          <MessageList scrollBehavior='smooth' typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing.." /> : null }>
             {messages.map((message, i) => (
               <Message key={i} model={message} />
             ))}
